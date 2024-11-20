9e7821106708f9cdff8d6c167e4b480b65b921c1
vaivaaaa <vaimaciulyte@gmail.com>
2024-11-20 14:01:55 +0200

    Added the Template pattern

--- SnakeMultiplayer/Services/ArenaBuilder.cs
+++ SnakeMultiplayer/Services/ArenaBuilder.cs
@@ -4,10 +4,10 @@ using SnakeMultiplayer.Services.Strategies.Movement;
 
 namespace SnakeMultiplayer.Services
 {
-    public class ArenaBuilder : IArenaBuilder
+    public abstract class ArenaBuilder : IArenaBuilder
     {
-        private readonly IArenaFactory _factory;
-        private Arena _arena;
+        protected readonly IArenaFactory _factory;
+        protected Arena _arena;
 
         public ArenaBuilder(IArenaFactory factory)
         {
@@ -20,80 +20,68 @@ namespace SnakeMultiplayer.Services
             return this;
         }
 
-        public IArenaBuilder SetSpeed(Speed speed)
+        public abstract IArenaBuilder SetSpeed(Speed speed);
+        public IArenaBuilder SetBoardSize(int width, int height)
+        {
+            _arena.CreateBoard(width, height);
+            return this;
+        }
+
+        public abstract IArenaBuilder AddObstacles(int count);
+
+        public Arena Build()
+        {
+            return _arena;
+        }
+
+        // Template method
+        public Arena ConstructArena(ConcurrentDictionary<string, Snake> players, int width, int height, int obstacleCount, Speed speed)
+        {
+            Start(players);
+            SetSpeed(speed);
+            SetBoardSize(width, height);
+            AddObstacles(obstacleCount);
+            return Build();
+        }
+    }
+
+
+    public class RandomArenaBuilder : ArenaBuilder
+    {
+        public RandomArenaBuilder(IArenaFactory factory) : base(factory) { }
+
+        public override IArenaBuilder SetSpeed(Speed speed)
+        {
+            Random random = new Random();
+            int speedCount = Enum.GetValues(typeof(Speed)).Length;
+            Speed randomSpeed = (Speed)random.Next(1, speedCount - 1);
+            _arena.Speed = randomSpeed;
+            return this;
+        }
+
+        public override IArenaBuilder AddObstacles(int count)
+        {
+            Random random = new Random();
+            int counRand = random.Next(5, 100);
+            _factory.CreateObstacles(_arena, counRand);
+            return this;
+        }
+    }
+
+    public class StandardArenaBuilder : ArenaBuilder
+    {
+        public StandardArenaBuilder(IArenaFactory factory) : base(factory) { }
+
+        public override IArenaBuilder SetSpeed(Speed speed)
         {
             _arena.Speed = speed;
             return this;
         }
 
-        public IArenaBuilder SetBoardSize(int width, int height)
-        {
-            _arena.CreateBoard(width, height);
-            return this;
-        }
-
-        public IArenaBuilder AddObstacles(int count)
+        public override IArenaBuilder AddObstacles(int count)
         {
             _factory.CreateObstacles(_arena, count);
             return this;
-        }
-
-        public Arena Build()
-        {
-
-            return _arena;
-        }
-    }
-
-
-    public class RandomArenaBuilder : IArenaBuilder
-    {
-        private readonly IArenaFactory _factory;
-        private Arena _arena;
-
-        public RandomArenaBuilder(IArenaFactory factory)
-        {
-            _factory = factory;
-        }
-
-        public IArenaBuilder Start(ConcurrentDictionary<string, Snake> players)
-        {
-            _arena = _factory.CreateArena(players);
-            return this;
-        }
-
-        public IArenaBuilder SetSpeed(Speed speed)
-        {
-
-             Random random = new Random();
-
-            int speedCount = Enum.GetValues(typeof(Speed)).Length;
-
-            Speed randomSpeed = (Speed)random.Next(1, speedCount-1);
-            _arena.Speed = randomSpeed;
-            return this;
-        }
-
-        public IArenaBuilder SetBoardSize(int width, int height)
-        {
-
-            _arena.CreateBoard(width, height);
-            return this;
-        }
-
-        public IArenaBuilder AddObstacles(int count)
-        {
-            Random random = new Random();
-    
-            int counRand = random.Next(5, 100);
-            
-            _factory.CreateObstacles(_arena, counRand);
-            return this;
-        }
-
-        public Arena Build()
-        {
-            return _arena;
         }
     }
 }
